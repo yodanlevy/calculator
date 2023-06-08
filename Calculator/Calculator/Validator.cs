79078af8b347f4729f1ffe7c7f285c36b05247a2
yodan <yodanlevy@mail.tau.ac.il>
2023-06-08 15:54:55 +0300

    Creat IsValid method

--- Calculator/Calculator/Validator.cs
+++ Calculator/Calculator/Validator.cs
@@ -5,7 +5,6 @@ namespace Calculator
     public class Validator
     {
         public string _e;
-
         public Validator(String equation)
         {
             _e = equation;
@@ -15,10 +14,8 @@ namespace Calculator
             while (string.IsNullOrWhiteSpace(_e))
             {
                 Console.WriteLine("Error");
-                _e= Console.ReadLine();
+                _e = Console.ReadLine();
             }
-            
-
         }
     }
 }
