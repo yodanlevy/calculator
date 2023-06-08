abca947b8c39b8af00df30bc8aeabab7628c33bf
yodan <yodanlevy@mail.tau.ac.il>
2023-06-09 00:41:11 +0300

    Create IsNull method

--- Calculator/Calculator/Validator.cs
+++ Calculator/Calculator/Validator.cs
@@ -4,19 +4,26 @@ namespace Calculator
 {
     public class Validator
     {
-        public string _equation;
-        public Validator(String equation)
+        private string _equation;
+        public Validator(string equation)
         {
             _equation = equation;
         }
         public void IsValid()
         {
-            while (string.IsNullOrWhiteSpace(_equation))
+            while (IsNull(_equation))
             {
                 Console.WriteLine("Error");
+                Console.WriteLine("Please enter new equation:");
                 _equation = Console.ReadLine();
             }
-
         }
+
+        public bool IsNull(string equation)
+        {
+            return string.IsNullOrWhiteSpace(_equation);
+        }
+
+
     }
 }
