bdc3c84bfe67a02a37fb876d0ee576abd2db3862
yodan <yodanlevy@mail.tau.ac.il>
2023-06-14 00:07:51 +0300

    Create IOperator

---
+++ Calculator/Calculator/IOperator.cs
@@ -0,0 +1,12 @@
+﻿namespace Calculator
+{
+    public interface IOperator
+    {
+        int num1 { get; set; }
+        int num2 { get; set; }
+        int priority { get; set; }
+        char operatorSign { get; set; }
+
+        int Calculate();
+    }
+}
