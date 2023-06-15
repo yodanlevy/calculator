e0e5507efe0c062266b26a11877904f6b4070d56
yodan <yodanlevy@mail.tau.ac.il>
2023-06-15 17:35:46 +0300

    Create Division

---
+++ Calculator/Calculator/Division.cs
@@ -0,0 +1,12 @@
+﻿namespace Calculator
+{
+    public class Division: Operator
+    {
+        public int Priority = 5;
+        public char OperatorSign = ':';
+        public override int Calculate(int leftOperand, int rightOperand)
+        {
+            return (leftOperand / rightOperand);
+        }
+    }
+}
