e0124f3558fb77654b1750ec4ae54e277f908835
yodan <yodanlevy@mail.tau.ac.il>
2023-06-15 17:27:03 +0300

    Create Multiplication

---
+++ Calculator/Calculator/Multiplication.cs
@@ -0,0 +1,12 @@
+﻿namespace Calculator
+{
+    public class Multiplication : Operator
+    {
+        public int Priority = 5;
+        public char OperatorSign = '*';
+        public override int Calculate(int leftOperand, int rightOperand)
+        {
+            return leftOperand * rightOperand;
+        }
+    }
+}
