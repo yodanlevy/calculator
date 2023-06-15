bf2bcccbe51777330c74e1bcf33ec655b0cda693
yodan <yodanlevy@mail.tau.ac.il>
2023-06-15 17:36:35 +0300

    Create Power

---
+++ Calculator/Calculator/Power.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Calculator
+{
+    public class Power : Operator
+    {
+        public int Priority = 4;
+        public char OperatorSign = '^';
+        public override int Calculate(int leftOperand, int rightOperand)
+        {
+            return (int)Math.Pow(leftOperand, rightOperand);
+        }
+    }
+}
