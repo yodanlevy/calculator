891514dcafc81fe727334f16cf844bb3ebdf192e
yodan <yodanlevy@mail.tau.ac.il>
2023-06-15 17:17:11 +0300

    Refactor IOpertaor to Operator, Create Addition

---
+++ Calculator/Calculator/Addition.cs
@@ -0,0 +1,14 @@
+﻿namespace Calculator
+{
+    public class Addition : Operator
+    {
+        public int priority = 6;
+        public char operatorSign = '+';
+        
+
+        public override int Calculate(int leftOperand, int rightOperand)
+        {
+            return rightOperand + rightOperand;
+        }
+    }
+}
---
+++ Calculator/Calculator/Operator.cs
@@ -0,0 +1,9 @@
+﻿namespace Calculator
+{
+    abstract class Operator
+    { 
+        public int Priority;
+        public char OperatorSign;   
+        public abstract int Calculate(int leftOperand, int rightOperand);
+    }
+}
