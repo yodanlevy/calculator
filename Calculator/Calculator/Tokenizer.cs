6ddc4efb8d1fc467dd75a6feebb19d983cc0e421
yodan <yodanlevy@mail.tau.ac.il>
2023-06-16 21:19:56 +0300

    Create Tokenizer

---
+++ Calculator/Calculator/Tokenizer.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Calculator
+{
+    public class Tokenizer
+    {
+        private List<char> operators = new List<char> { '+', '-', '*', ':', '^' };
+        private List<char> openParentheses = new List<char> { '(', '{', '[' };
+        private List<char> closeParentheses = new List<char> { ')', '}', ']' };
+        private List<int> numbers = new List<int> { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+    }
+}
