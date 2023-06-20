183ed14ab5146da271eda4c63d9de4e6cbd1e21e
yodan <yodanlevy@mail.tau.ac.il>
2023-06-20 19:59:10 +0300

    Create Calculate

---
+++ Calculator/Calculator/ALU.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Runtime.Remoting;
+
+namespace Calculator
+{
+    public class ALU
+    {
+        public int Calculate(List<object> components)
+        {
+            int leftOperand = 0;
+            for (int i = 0; i < components.Count; i++)
+            {
+                if (i == components.Count)
+                {
+                    return (int) components[i];
+                }
+
+                if (components[i] is Operator)
+                {
+                    var slicedEquation = components.GetRange(i+1, components.Count - 1);
+                    var rightOperand = Calculate((List<object>)slicedEquation);
+                    var op = (Operator) components[i];
+
+                    return op.Calculate(leftOperand, rightOperand);
+                }
+
+                if (components[i] is int)
+                {
+                    leftOperand = (int) components[i];
+                }
+            }
+
+            return 1;
+        }
+    }
+}
