f536e3f652b09e687b34492bba5d5bab857a7f54
yodan <yodanlevy@mail.tau.ac.il>
2023-06-26 19:13:58 +0300

    Change Calculate

--- Calculator/Calculator/ALU.cs
+++ Calculator/Calculator/ALU.cs
@@ -17,15 +17,22 @@ namespace Calculator
             int leftOperand = 0;
             for (int i = 0; i < components.Count; i++)
             {
-                isJumpToCurrentIndex(i, general_result);
-                general_result = isEndOfEquation(general_result, i, components);
-                leftOperand = isComponentInt(components, i, leftOperand);
-                general_result = isComponentOperator(components, leftOperand, i, general_result);
+                if (i == components.Count - 1)
+                {
+                    general_result.value = (int)components[i];
+                    return general_result;
+                }
 
-                return general_result;
+                IsJumpToCurrentIndex(i, general_result);
+                leftOperand = IsComponentInt(components, i, leftOperand);
+
+                if (components[i] is Operator)
+                {
+                    general_result = IsComponentOperator(components, leftOperand, i, general_result);
+                    return general_result;
+
+                }
             }
-
-            general_result.isNull = true;
 
             return general_result;
         }
@@ -33,38 +40,33 @@ namespace Calculator
 
 
 
-        private int isJumpToCurrentIndex(int i, Result result)
+        private void IsJumpToCurrentIndex(int currentIndex, Result result)
         {
             if (result.isNull)
             {
-                i = currentOperatorIndex;
+                currentIndex = currentOperatorIndex;
             }
-
-            return i;
         }
 
-        private Result isComponentOperator(List<object> components, int leftOperand, int currentIndex, Result result)
+        private Result IsComponentOperator(List<object> components, int leftOperand, int currentIndex, Result result)
         {
-            if (components[currentIndex] is Operator)
+            var op = (Operator)components[currentIndex];
+
+            if (op.Priority < priority)
             {
-                var op = (Operator)components[currentIndex];
-
-                if (op.Priority < priority)
-                {
-                    currentOperatorIndex = currentIndex;
-                    result.isNull = true;
-                }
-
-                priority = op.Priority;
-                var slicedEquation = components.GetRange(currentIndex + 1, components.Count - currentIndex - 1);
-                var rightOperand = Calculate((List<object>)slicedEquation);
-                result.value = op.Calculate(leftOperand, rightOperand.value);
+                currentOperatorIndex = currentIndex;
+                result.isNull = true;
             }
+
+            priority = op.Priority;
+            var slicedEquation = components.GetRange(currentIndex + 1, components.Count - currentIndex - 1);
+            var rightOperand = Calculate((List<object>)slicedEquation);
+            result.value = op.Calculate(leftOperand, rightOperand.value);
 
             return result;
         }
 
-        private int isComponentInt(List<object> components, int currentIndex, int leftOperand)
+        private int IsComponentInt(List<object> components, int currentIndex, int leftOperand)
         {
             if (components[currentIndex] is int)
             {
@@ -74,7 +76,7 @@ namespace Calculator
             return leftOperand;
         }
 
-        private Result isEndOfEquation(Result result, int currentIndex, List<object> components)
+        private Result IsEndOfEquation(Result result, int currentIndex, List<object> components)
         {
             if (currentIndex == components.Count - 1)
             {
