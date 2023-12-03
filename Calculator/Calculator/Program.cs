5de1a56a852c79adcd52c88e67c523aab2baa87f
yodan <yodanlevy@mail.tau.ac.il>
2023-12-04 01:10:05 +0200

    Remove empty line

--- Calculator/Calculator/Program.cs
+++ Calculator/Calculator/Program.cs
@@ -8,14 +8,13 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
-            var equation = "2*3-2+2+4/2";
             Tokenizer tokenizer = new Tokenizer();
+            var equation = "3^3-2*3+10/2";
             var equationTokens = tokenizer.Tokenize(equation);
 
             ALU alu = new ALU();
             var result = alu.Calculate(equationTokens);
             Console.WriteLine("Answer: " + result.value);
-
         }
     }
 }
